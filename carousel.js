bc6341876c1dd47b03cf5423d13069a8e0989bea
Julia Rykova <yuliya.yurevna.01@inbox.ru>
2024-01-30 13:56:00 +0300

    done

--- carousel.js
+++ carousel.js
@@ -1,8 +1,8 @@
-let i = 1;
+let k = 1;
 for (let li of carousel.querySelectorAll('li')) {
     li.style.position = 'relative';
-    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-    i++;
+    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${k}</span>`);
+    k++;
 }
 
 let width = 130;
@@ -29,36 +29,3 @@ carousel.querySelector('.next').onclick = function () {
     }
     list.style.marginLeft = position + 'px';
 };
-
-// function carouselFunction() {
-//     let i = 1;
-//     for (let li of carousel.querySelectorAll('li')) {
-//         li.style.position = 'relative';
-//         li.insertAdjacentHTML('beforeend', <span style="position:absolute;left:0;top:0">${i}</span>);
-//         i++;
-//     }
-//     let width = 100; // ширина картинки
-//     let count = 6; // видимое количество изображений
-//     let list = carousel.querySelector('ul');
-//     let listElems = carousel.querySelectorAll('li');
-
-//     let position = 0;
-//     carousel.querySelector('.prev').onclick = function () {
-//         position += width;
-//         position = Math.min(position, 0);
-//         if (position === 0) {
-//             position = -width * (listElems.length - count);
-//         }
-//         list.style.marginLeft = position + 'px';
-//     };
-//     carousel.querySelector('.next').onclick = function () {
-//         position -= width;
-//         position = Math.max(position, -width * (listElems.length - count));
-//         if (position === -width * (listElems.length - count)) {
-//             position = 0;
-//         }
-//         list.style.marginLeft = position + 'px';
-//     };
-// }
-
-// carouselFunction();
